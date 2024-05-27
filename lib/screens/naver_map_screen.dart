60c435af63dc5f66170a2481758f8195dc547e27
yoojin282 <yoojin282@gmail.com>
2024-05-27 09:32:38 +0900

    flutter_naver_map 위젯변환 오류 재현

--- lib/screens/naver_map_screen.dart
+++ lib/screens/naver_map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class NaverMapScreen extends StatefulWidget {
   const NaverMapScreen({super.key});
@@ -10,16 +11,63 @@ class NaverMapScreen extends StatefulWidget {
 
 class _NaverMapScreenState extends State<NaverMapScreen> {
   late final NaverMapController _mapController;
+
+  void _onMapCreated(NaverMapController controller) {
+    _mapController = controller;
+
+    NOverlayImage.fromWidget(
+            widget: const _Overlay("흐리고 비"),
+            size: const Size(100, 200),
+            context: context)
+        .then(
+      (overlayImage) {
+        return controller.addOverlay(
+          NMarker(
+            id: "sample1",
+            size: const Size(100, 200),
+            position: const NLatLng(37, 127),
+            icon: overlayImage,
+            // anchor: const NPoint(0.1, 0.7),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: NaverMap(
-        onMapReady: (controller) => _mapController = controller,
+        onMapReady: _onMapCreated,
         options: const NaverMapViewOptions(
           initialCameraPosition:
               NCameraPosition(target: NLatLng(37, 127), zoom: 14),
         ),
       ),
+    );
+  }
+}
+
+class _Overlay extends StatelessWidget {
+  const _Overlay(this.text);
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          text,
+          style: const TextStyle(
+            fontSize: 18,
+            color: Colors.black,
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        // SvgPicture.asset('images/cloud_rain.svg'),
+        Image.asset("images/cloud_rain.png"),
+      ],
     );
   }
 }
